5a7c8aea533924f2208bc1ae4d20ab73de227ebb
Christian Carvelli <chca@itu.dk>
2019-02-01 13:06:54 +0100

    Random FrozenLake with console param

--- tests/frozenLake/test.py
+++ tests/frozenLake/test.py
@@ -5,9 +5,23 @@ import gym
 @click.command()
 @click.option("--n_generations", type=int, default=10000)
 @click.option("--n_processes", type=int, default=1)
-def run(n_generations, n_processes):
+@click.option("--render", is_flag=True)
+def run(n_generations, n_processes, render):
     env = gym.make('FrozenLake-v0')
     print(env.action_space)
+    print(env.observation_space)
+
+    obs = env.reset()
+
+    tot_reward = 0
+    done = False
+    while not done:
+        obs, reward, done, _ = env.step(env.action_space.sample())
+        if render:
+            env.render()
+
+        tot_reward += reward
+    print(tot_reward)
 
 
 if __name__ == "__main__":
