8a92675d5f9a7eb4b6eeef70f093c6a2228793a7
Rasmus Ravn Frost <rasmus.ravn.frost@gmail.com>
2019-02-08 23:16:14 +0100

    Generalized the choice env to work with any size

--- custom_envs/envs/choice.py
+++ custom_envs/envs/choice.py
@@ -1,19 +1,18 @@
-
 from gym_minigrid.minigrid import *
-from gym_minigrid.register import register
 
-
-class ChoiceEnv3x1(MiniGridEnv):
+class ChoiceEnv(MiniGridEnv):
     """
     Empty grid environment, no obstacles, sparse reward
     """
 
-    def __init__(self, goal_pos):
+    def __init__(self, goal_pos, width=3, height=1):
         self.goal_pos = goal_pos % 2
+        assert width >= 3
+        assert height >= 1
         super().__init__(
-            width=3 + 2,
-            height=1 + 2,
-            max_steps=2,
+            width=width + 2,
+            height=height + 2,
+            max_steps=2 * (width + height),
             # Set this to True for maximum speed
             see_through_walls=True
         )
@@ -26,7 +25,7 @@ class ChoiceEnv3x1(MiniGridEnv):
         self.grid.wall_rect(0, 0, width, height)
 
         # Place the agent in the top-left corner
-        self.start_pos = (2, 1)
+        self.start_pos = (int(width/2), height - 2)
         self.start_dir = 0
 
         i_left = self.goal_pos
@@ -34,27 +33,30 @@ class ChoiceEnv3x1(MiniGridEnv):
 
         left = Goal()
         left.color = IDX_TO_COLOR[1 + i_left]  # color
-        left.type = IDX_TO_OBJECT[7 + i_left]  # Box or Goal
+        left.type = IDX_TO_OBJECT[8 - i_left]  # Box or Goal
 
         right = Goal()
         right.color = IDX_TO_COLOR[1 + i_right]  # color
-        right.type = IDX_TO_OBJECT[7 + i_right]  # Box or Goal
+        right.type = IDX_TO_OBJECT[8 - i_right]  # Box or Goal
 
         # Place a goal square in the bottom-right corner
         self.grid.set(1, 1, left)
-        self.grid.set(3, 1, right)
+        self.grid.set(width - 2, 1, right)
         self.mission = "Get to the goal"
         self.goal_id = self.goal_pos
 
+    def _reward(self):
+        """
+        Compute the reward to be given upon success
+        """
+        return 1 - 0.9 * min(1, (self.step_count / self.max_steps))
 
-class ChoiceEnv3x1_0(ChoiceEnv3x1):
+
+class ChoiceEnv3x1_0(ChoiceEnv):
     def __init__(self):
-        super().__init__(0)
+        super().__init__(0, 3, 1)
 
 
-class ChoiceEnv3x1_1(ChoiceEnv3x1):
+class ChoiceEnv3x1_1(ChoiceEnv):
     def __init__(self):
-        super().__init__(1)
-
-# i need this https://github.com/MartinThoma/banana-gym/tree/master/gym_banana
-
+        super().__init__(1, 3, 1)
--- custom_envs/example.py
+++ custom_envs/example.py
@@ -1,16 +1,25 @@
 import gym
 import custom_envs
 
-# env_name = "MiniGrid-Choice3x1-pos0-v0"
-env_name = "MiniGrid-Choice3x1-pos1-v0"
-fps = 5
+# env_name = "MiniGrid-Choice3x1-pos0-v0"  # goal is at the left
+env_name = "MiniGrid-Choice3x1-pos1-v0"  # goal is at the right
+fps = 8
 
 env = gym.make(env_name)
+
+# # alternatively envs can be created manually with custom size like this
+# from custom_envs.envs.choice import ChoiceEnv
+# env = ChoiceEnv(0, 5, 5)
 
 # show env
 env.reset()
 import time
-for _ in range(10000000):
+while True:
     env.render()
     step = observation, reward, done, info = env.step(env.action_space.sample())
     time.sleep(1.0 / fps)
+    if reward > 0:
+        print("GOAL with reward: " + str(reward))
+        time.sleep(3)
+        break
+
