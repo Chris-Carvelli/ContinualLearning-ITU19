19f9b5437f9f0c848a666be23e8f80a48748f7f9
Luka Locniskar <luka.locniskar@gmail.com>
2019-02-22 01:54:21 +0100

    continuing on runner

--- tests/minigrid/runner.py
+++ tests/minigrid/runner.py
@@ -1,18 +1,20 @@
 from tests.minigrid.ga import GA
 import gym_minigrid
+from sessions.session import Session
 
-for runs in experiments:
-        print('main')
+class Experiment:
+    def __init__(self, config_file_path, number_of_runs):
+        self.config_file_path = config_file_path
+        self.number_of_runs = number_of_runs
+        self.ga = GA(config_file_path)
 
-        ga = GA()
+    def iterate(self):
+        runs = 0
+        while runs < self.number_of_runs:
+            #TODO talk with rasmus about this, when will session end, does it freeze the thread, how is it being logged
+            Session(self.ga, "Experiment: "+str(runs))
+            runs = runs + 1
+            #TODO Reset GA
 
-        pickle.dump(ga, open('ga.pickle', 'wb'))
-        fp = open('process.pickle', 'ab')
-        while True:
-                try:
-                        res = ga.optimize()
-                        print(f'done with generation{res[0]}')
-                        pickle.dump(res, fp)
-                except StopIteration:
-                        break
-        fp.close()
+
+first_experiment = Experiment('config_files/config_one', 5)
