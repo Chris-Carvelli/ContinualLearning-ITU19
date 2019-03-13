51be62261a7baffb67d875f8f604c5fec9b0427d
Luka Locniskar <luka.locniskar@gmail.com>
2019-03-13 13:41:09 +0100

    removed original ga init function

--- runner.py
+++ runner.py
@@ -3,6 +3,6 @@ from sessions.session import Session, MultiSession
 
 # Example of MultiSession
 config_one = 'config_files/config_one'
-config_fast = 'config_files/config_fast'
+config_fast = 'config_files/config_faster'
 ms = MultiSession([GA(config_fast), GA(config_fast), GA(config_fast)], "TestSessions")
 ms.start()
--- src/ga.py
+++ src/ga.py
@@ -4,24 +4,125 @@ import random
 import numpy as np
 import src.ControllerFactory as mf
 from configparser import ConfigParser
-
+from src.ControllerFactory import *
+from custom_envs import *
 
 termination_strategies = \
     {
         'all_generations': lambda ga_instance: ga_instance.g < ga_instance.max_generations,
     }
+model_library = \
+    {
+        'ntm': lambda copy_size: builder_ntm(copy_size),
+        'base': lambda: builder_base(),
+        'hyper': lambda: builder_hyper()
+    }
 
 # TODO Make elite strategies dict and parent selection strategies dict
-elite_strategies =\
+elite_strategies = \
     {
 
     }
-parent_selection_strategies =\
+parent_selection_strategies = \
     {
     }
-
-
 class GA:
+    def __init__(self, config_file_path=None,
+                 env_key=None,
+                 population=None,
+                 model_builder=None,
+                 max_generations=None,
+                 max_evals=None,
+                 max_episode_eval=None,
+                 sigma=None,
+                 truncation=None,
+                 trials=None,
+                 elite_trials=None,
+                 n_elites=None,
+                 hyper_mode=None):
+        config = ConfigParser()
+        if config_file_path is not None:
+            config.read(config_file_path)
+        else:
+            config.read('config_files/config_default')
+        self.config_file = config
+
+        if model_builder is None:
+            self.model_builder = model_library[config["Controller"]["model_builder"]]
+        else:
+            self.model_builder = model_builder
+
+        if population is None:
+            self.population = int(config["HyperParameters"]["population"])
+        else:
+            self.population = population
+
+        if max_episode_eval is None:
+            self.max_episode_eval = int(config["HyperParameters"]["max_episode_eval"])
+        else:
+            self.max_episode_eval = max_episode_eval
+
+        if max_evals is None:
+            self.max_evals = int(config["HyperParameters"]["max_evals"])
+        else:
+            self.max_evals = max_evals
+
+        if max_generations is None:
+            self.max_generations = int(config["HyperParameters"]["max_generations"])
+        else:
+            self.max_generations = max_generations
+
+        if sigma is None:
+            self.sigma = float(config["HyperParameters"]["sigma"])
+        else:
+            self.sigma = sigma
+
+        if truncation is None:
+            self.truncation = int(config["HyperParameters"]["truncation"])
+        else:
+            self.truncation = truncation
+
+        if trials is None:
+            self.trials = int(config["HyperParameters"]["trials"])
+        else:
+            self.trials = trials
+
+        if elite_trials is None:
+            self.elite_trials = int(config["HyperParameters"]["elite_trials"])
+        else:
+            self.elite_trials = elite_trials
+
+        if n_elites is None:
+            self.n_elites = int(config["HyperParameters"]["n_elites"])
+        else:
+            self.n_elites = n_elites
+
+        if hyper_mode is None:
+            self.hyper_mode = bool(int(config["HyperParameters"]["hyper_mode"]))
+        else:
+            self.hyper_mode = hyper_mode
+
+        if env_key is None:
+            self.env_key = str(config["EnvironmentSettings"]["env_key"])
+        else:
+            self.env_key = env_key
+
+        print(self.env_key)
+
+        self.scored_parents = None
+        self.models = self.init_models()
+
+        self.termination_strategy_name = config["Strategies"]["termination"]
+        # TODO Make Other strategies to be modular
+
+        # algorithm state
+        self.g = 0
+        self.evaluations_used = 0
+        self.env = gym.make(self.env_key)
+
+        # results TMP check if needed
+        self.results = []
+    '''
     def __init__(self, env_key, population, model_builder,
                  networked=False,
                  max_generations=20,
@@ -61,43 +162,8 @@ class GA:
         self.evaluations_used = 0
         self.env = gym.make(self.env_key)
 
-        self.results = []
-
-    def __init__(self, config_file_path):
-        config = ConfigParser()
-        config.read(config_file_path)
-        self.config_file = config
-        self.model_builder = mf.builder_base
-        # self.networked = bool(int(config["Utility"]["networked"]))
-        self.population = int(config["HyperParameters"]["population"])
-        self.max_episode_eval = int(config["HyperParameters"]["max_episode_eval"])
-        self.max_evals = int(config["HyperParameters"]["max_evals"])
-        self.max_generations = int(config["HyperParameters"]["max_generations"])
-        self.sigma = float(config["HyperParameters"]["sigma"])
-        self.truncation = int(config["HyperParameters"]["truncation"])
-        self.trials = int(config["HyperParameters"]["trials"])
-        self.elite_trials = int(config["HyperParameters"]["elite_trials"])
-        self.n_elites = int(config["HyperParameters"]["n_elites"])
-        self.hyper_mode = bool(int(config["HyperParameters"]["hyper_mode"]))
-
-        self.env_key = str(config["EnvironmentSettings"]["env_key"])
-
-        print(self.env_key)
-
-        self.scored_parents = None
-        self.models = self.init_models()
-
-        self.termination_strategy_name = config["Strategies"]["termination"]
-        # TODO Make Other strategies to be modular
-
-        # algorithm state
-        self.g = 0
-        self.evaluations_used = 0
-        self.env = gym.make(self.env_key)
-
-        # results TMP check if needed
-        self.results = []
-
+        self.results = [] 
+    '''
     def iterate(self):
         if termination_strategies[self.termination_strategy_name](self):
             if self.models is None:
@@ -153,7 +219,7 @@ class GA:
         scored_models = self.score_models(models, trials)
 
         self.evaluations_used += sum([sum(map(lambda x: x[1], xs)) for (_, xs) in scored_models])
-        scored_models = [(m, sum(map(lambda x: x[0], xs))/float(len(xs))) for m, xs in scored_models]
+        scored_models = [(m, sum(map(lambda x: x[0], xs)) / float(len(xs))) for m, xs in scored_models]
 
         scored_models.sort(key=lambda x: x[1], reverse=True)
 
@@ -192,7 +258,7 @@ class GA:
     def __getstate__(self):
         state = self.__dict__.copy()
 
-        del state['Controllers']
+        #        del state['Controllers']
 
         return state
 
