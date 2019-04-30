852e372fbc26c626bcc5fdfb4ab1057d7a8d0b70
Rasmus Ravn Frost <rasmus.ravn.frost@gmail.com>
2019-04-30 12:17:33 +0200

    fixed error in configuration

--- custom_envs/__init__.py
+++ custom_envs/__init__.py
@@ -64,9 +64,9 @@ def _register_envs():
         kwargs=dict(corridor_length=2, rounds_pr_side=4, view_size=3, uneven_rounds=True)
     )
     register(
-        id=f"TMaze-2x5-3-UnevenRounds-x2-v0",
+        id=f"TMaze-2x4-3-UnevenRounds-x2-v0",
         entry_point='custom_envs.envs:TMaze',
-        kwargs=dict(corridor_length=2, rounds_pr_side=5, view_size=3, uneven_rounds=True, repeat=2)
+        kwargs=dict(corridor_length=2, rounds_pr_side=4, view_size=3, uneven_rounds=True, repeat=2)
     )
 
     for rounds in (1, 2, 3, 4, 5, 6, 10, 20, 50, 100, 200, 500, 1000):
